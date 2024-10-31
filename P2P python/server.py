1e8bac5c29bf6bd8831fc4002cb9990161180e31
Gabriel-Nog <nogueirajoao799@gmail.com>
2024-10-31 17:28:04 -0300

    Novas mudanças no server

--- P2P python/server.py
+++ P2P python/server.py
@@ -29,7 +29,7 @@ async def handler(websocket, path):
     finally:
         connected_clients.remove(websocket)
 
-start_server = websockets.serve(handler, "localhost", 8765)
+start_server = websockets.serve(handler, "0.0.0.0", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
